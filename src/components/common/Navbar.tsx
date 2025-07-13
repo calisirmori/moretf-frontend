07c049c6824b1d5375784fcd49349a1467cb4fee
Mori Calisir <104592697+calisirmori@users.noreply.github.com>
2025-07-12 22:13:15 -0700

    extra unused

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import { useTheme } from "../../contexts/ThemeContext";
-import { useState } from "react";
 
 export default function Navbar() {
   const { pathname } = useLocation();
